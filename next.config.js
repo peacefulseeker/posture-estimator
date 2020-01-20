ede7433ed6d60de8b37c30d9f8b1d2f9bfa409fc
Alexey Vorobyov <alexey.vorobyov@prezi.com>
2020-01-20 20:52:36 +0200

    Add polyfills loader to next config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,21 @@
 const withImages = require('next-images');
-module.exports = withImages();
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins([
+    withImages,
+], {
+    webpack(webpackConfig) {
+        const originalEntry = webpackConfig.entry;
+        webpackConfig.entry = async() => {
+            const entries = await originalEntry();
+            if (entries['main.js'] && !entries['main.js'].includes('./util/polyfills.js')) {
+                entries['main.js'].unshift('./util/polyfills.js');
+            }
+
+            return entries;
+        };
+
+
+        return webpackConfig;
+    },
+});
